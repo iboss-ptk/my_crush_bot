0ce98c97e01f5463fb350804e314ef425f721b2d
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2018-08-13 17:51:54 +0700

    Create line module

---
+++ lib/line/client.ex
@@ -0,0 +1,25 @@
+defmodule Line.Client do
+  @reply_endpoint "https://api.line.me/v2/bot/message/reply"
+  @line_conf Application.get_env(:my_crush_bot, Line)
+
+  def reply(messages, replyToken) do
+    HTTPoison.post(
+      @reply_endpoint,
+      Poison.encode!(%{
+        "replyToken" => replyToken,
+        "messages" => messages
+      }),
+      [
+        {"Content-Type", "application/json"},
+        {"Authorization", "Bearer #{@line_conf[:channel_access_token]}"}
+      ]
+    )
+  end
+
+  def text(text_string) do
+    %{
+      "type" => "text",
+      "text" => text_string
+    }
+  end
+end
